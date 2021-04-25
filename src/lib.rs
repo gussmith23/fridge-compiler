e4df876315f264068b25730ea8655e3034c72a57
Gus Smith <guscomps@gmail.com>
2021-04-24 17:52:17 -0700

    DeMorgan rewrites push nots to leaves

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use egg::{define_language, Id, RecExpr, Symbol};
+use egg::{define_language, rewrite, Id, RecExpr, Rewrite, Symbol};
 
 define_language! {
     pub enum Language {
@@ -19,12 +19,118 @@ pub fn interpret(expr: &RecExpr<Language>, id: Id, env: &HashMap<Symbol, bool>) 
         Language::Symbol(s) => *env.get(&s).expect("Symbol not found in environment!"),
     }
 }
+
+pub fn demorgans_and() -> Rewrite<Language, ()> {
+    rewrite!("demorgans-and"; "(not (and ?a ?b))" => "(or (not ?a) (not ?b))")
+}
+
+pub fn demorgans_or() -> Rewrite<Language, ()> {
+    rewrite!("demorgans-or"; "(not (or ?a ?b))" => "(and (not ?a) (not ?b))")
+}
+
+pub fn simplify_double_not() -> Rewrite<Language, ()> {
+    rewrite!("simplify-double-not"; "(not (not ?a))" => "?a")
+}
+
 #[cfg(test)]
 mod tests {
 
+    use egg::{CostFunction, EGraph, Runner};
+
     use super::*;
 
-    /// This test compares two versions of the same program to make sure they're equivalent.
+    /// Tests whether DeMorgan rewrites successfully push the Nots to the leaves
+    /// of the program.
+    #[test]
+    fn push_nots_to_leaves_0x87() {
+        let mut expr = RecExpr::default();
+        let a_id = expr.add(Language::Symbol(Symbol::from("A")));
+        let b_id = expr.add(Language::Symbol(Symbol::from("B")));
+        let c_id = expr.add(Language::Symbol(Symbol::from("C")));
+        let not_a_id = expr.add(Language::Not([a_id]));
+        let not_b_id = expr.add(Language::Not([b_id]));
+        let not_c_id = expr.add(Language::Not([c_id]));
+        let g1_id = expr.add(Language::And([not_a_id, not_b_id]));
+        let not_g1_id = expr.add(Language::Not([g1_id]));
+        let g2_id = expr.add(Language::And([not_g1_id, not_c_id]));
+        let not_g2_id = expr.add(Language::Not([g2_id]));
+        let g3_id = expr.add(Language::And([not_g1_id, not_g2_id]));
+        let g4_id = expr.add(Language::And([not_g2_id, not_c_id]));
+        let _x_id = expr.add(Language::Or([g3_id, g4_id]));
+
+        let mut egraph = EGraph::new(());
+        let x_id_in_egraph = egraph.add_expr(&expr);
+
+        let runner = Runner::<_, _, ()>::new(()).with_egraph(egraph).run(&vec![
+            demorgans_and(),
+            demorgans_or(),
+            simplify_double_not(),
+        ]);
+
+        runner.print_report();
+        /// this [`CostFunction`] helps us determine if all nots have been
+        /// pushed to the leaves of the program.
+        struct OnlyAllowNotsOnLeaves;
+
+        /// This cost enum conveys whether this is an enode where [`Not`]s
+        /// appear anywhere other than on literals.
+        #[derive(Debug, PartialOrd, PartialEq, Clone)]
+        enum OnlyAllowNotsOnLeavesCost {
+            /// Represents the case where an enode is a symbol, and thus it is
+            /// trivially true that [`Not`]s don't appear on anything but
+            /// symbols in this expression (because this expression is just a
+            /// single symbol!)
+            IsSymbol,
+            /// Represents the case where an enode is not a symbol, in which
+            /// case, the boolean represents whether this expression contains
+            /// [`Language::Not`]s on anything other than symbols.
+            IsNotSymbol(bool),
+        }
+
+        impl CostFunction<Language> for OnlyAllowNotsOnLeaves {
+            type Cost = OnlyAllowNotsOnLeavesCost;
+
+            fn cost<C>(&mut self, enode: &Language, mut costs: C) -> Self::Cost
+            where
+                C: FnMut(Id) -> Self::Cost,
+            {
+                match enode {
+                    Language::Symbol(_) => OnlyAllowNotsOnLeavesCost::IsSymbol,
+                    Language::Not([id]) => match costs(*id) {
+                        OnlyAllowNotsOnLeavesCost::IsSymbol => {
+                            OnlyAllowNotsOnLeavesCost::IsNotSymbol(false)
+                        }
+                        _ => OnlyAllowNotsOnLeavesCost::IsNotSymbol(true),
+                    },
+                    Language::And(ids) | Language::Or(ids) => {
+                        OnlyAllowNotsOnLeavesCost::IsNotSymbol(
+                            ids.iter()
+                                .map(|id| costs(*id))
+                                .map(|cost| match cost {
+                                    OnlyAllowNotsOnLeavesCost::IsSymbol => false,
+                                    OnlyAllowNotsOnLeavesCost::IsNotSymbol(b) => b,
+                                })
+                                .reduce(|a, b| a || b)
+                                .unwrap(),
+                        )
+                    }
+                }
+            }
+        }
+
+        // Assert that there exists some expression which contains nots only at
+        // the leaves.
+        let result =
+            egg::Extractor::new(&runner.egraph, OnlyAllowNotsOnLeaves).find_best(x_id_in_egraph);
+        println!("{}", result.1);
+        assert!(match result.0 {
+            OnlyAllowNotsOnLeavesCost::IsNotSymbol(true) => false,
+            _ => true,
+        });
+    }
+
+    /// This test compares two versions of the same program to make sure they're
+    /// equivalent.
     #[test]
     fn example_0x87() {
         let mut expr = RecExpr::default();
